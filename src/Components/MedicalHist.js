315ceab94bde7a92af808094502e3798c5e315d6
wolvecode <wolvecode@gmail.com>
2020-01-25 21:25:12 +0100

    make changes in Medical history component and some custom styles

--- src/Components/MedicalHist.js
+++ src/Components/MedicalHist.js
@@ -1,33 +1,38 @@
 import React from "react";
-import {Tabs, Tab, Col, Container, Form} from "react-bootstrap";
+import {Tabs, Tab, Col, Container,Row, Sonnet} from "react-bootstrap";
 import "../css/MedicalHist.css";
 
 class MedicalHist extends React.Component{
     render() {
 
         return(
-            <Container fluid={1} className="content">
-                <Col  md={{span: 9, offset: 3}} className="tab tab-s">
-                    <Tabs className="tab-i" defaultActiveKey="home" id="uncontrolled-tab-example">
-                        <Tab class="tab-l active" eventKey="home" title="Medical History">
-                            <Col md={{span: 8, offset: 2}}>
-                                <Form>
-                                    <h4>Medical History For Azeez Biodun Habeeb</h4>
-                                    <hr/>
-                                    ghjhfdkjfdfdkj
-                                </Form>
-                            </Col>
-                        </Tab>
-                        <Tab eventKey="profile" title="New History">
-                            <h4>Medical History For Azeez Biodun</h4>
-                            <hr/>
-                            <h4>15,Nov 2019</h4>
-                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium distinctio doloribus dolorum, eum fuga illo iste maxime molestiae nam necessitatibus nemo nesciunt nobis odio optio quas sint soluta sunt veniam veritatis? Asperiores corporis facere labore nam nemo non nostrum, reiciendis? Consectetur, cumque fuga incidunt libero minima provident ratione repellat.</h5>
-                        </Tab>
-
-                    </Tabs>
-                    <hr/>
-                </Col>
+            <Container fluid={1}>
+                <Row className="user" >
+                    <Col>
+                        <p>
+                            <i className="fa fa-user" aria-hidden="true">
+                            </i>
+                            he
+                        </p>
+                    </Col>
+                    <Col>
+                        Welcome
+                    </Col>
+                </Row>
+                <Row>
+                    <div>
+                        <nav className="tabs tabs-style-line">
+                            <ul>
+                                <li>
+                                    Key
+                                </li>
+                                <li>
+                                    Home
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </Row>
             </Container>
         )
     }
