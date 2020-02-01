a2b3eb313bd6cfe3662de0422f286be6153f6675
wolvecode <wolvecode@gmail.com>
2020-02-01 11:55:26 +0100

    Bill user nav done

---
+++ src/Components/User.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../css/User.css";
+
+class User extends React.Component{
+    render() {
+        return(
+            <div className="row nav-row">
+                <div className="col-lg-12 p-0">
+                    <ul className="nav p-2">
+                        <li className="nav-item">
+                            <i className="fa fa-user" /> Azeez Biodun Habeeb
+                        </li>
+                        <li className="nav-item ml-auto">
+                            <i className="fa fa-sign-in" /> Sign as Azeez Biodun Habeeb
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+export default User;
